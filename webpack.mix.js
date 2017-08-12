3616a31e4903cf7410f71c20ac81da4f8e336d35
Shirimasen <pocosiemeczyc@gmail.com>
2017-08-12 18:31:31 +0200

    Probably final mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
 let mix = require('laravel-mix');
 
+mix.disableNotifications();
+mix.options({
+	processCssUrls: false
+});
+
 mix.js('js/kanpeki.js', 'dist');
 mix.sass('scss/kanpeki.scss', 'dist', {
 	outputStyle: 'expanded',
 	precision: 6
-});
-
-mix.disableNotifications();
-mix.options({
-	processCssUrls: false
 });
